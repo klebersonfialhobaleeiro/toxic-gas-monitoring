0a1b115b824ca6a609ada1d8b7c21094f53ed013
jaderAdriel <jaderAdriel7@gmail.com>
2023-01-18 20:11:33 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { homepage as base } from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 })
